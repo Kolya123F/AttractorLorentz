dab46afe8dde0348dfd7110d4ec778c4374b7835
Kolya123F <kolyatodorov12@gmail.com>
2026-04-15 21:31:22 +0300

    Сделал красивее

--- Маятник.py
+++ Маятник.py
@@ -2,29 +2,7 @@ import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 import numpy as np
 
-x_01 = 1
-y_01 = 1
-z_01 = 1
-
-x_02 = 1 + 0.00001
-y_02 = 1
-z_02 = 1
-
-number = 40000
-
-x1 = np.zeros(number)
-y1 = np.zeros(number)
-z1 = np.zeros(number)
-
-x2 = np.zeros(number)
-y2 = np.zeros(number)
-z2 = np.zeros(number)
-
-sol1 = np.vstack(([x1, y1, z1]))
-sol2 = np.vstack([x2, y2, z2])
-
-t = np.linspace(0, 100, number)
-
+#Правая часть системы диффуров
 def f1(sigma, x, y, z):
     return sigma * (y - x)
 
@@ -37,46 +15,44 @@ def f3(b, x, y, z):
 def f(sigma, r, b, x, y, z):
     return np.array([f1(sigma, x, y, z), f2(r, x, y, z), f3(b, x, y, z)])
 
+#Начальные условия
+x_01 = 1
+y_01 = 1
+z_01 = 1
+
+x_02 = 1 + 0.00001
+y_02 = 1
+z_02 = 1
+
+#Число точек
+number = 40000
+
+#Длительность
+T = 100
+
+#Параметры системы
 sigma, r, b = 10, 28, 8/3
-h = t[1] - t[0]
 
-x1[0], y1[0], z1[0] = x_01, y_01, z_01
-x2[0], y2[0], z2[0] = x_02, y_02, z_02
+#Решаем численно систему диффуром методом Рунге-Кутта
+def solve(sigma, r, b, x0, y0, z0, number, T):
+    t = np.linspace(0, T, number)
+    h = t[1] - t[0]
+    sol = np.zeros((3, number))
+    sol[0, 0] = x0
+    sol[1, 0] = y0
+    sol[2, 0] = z0
+    for i in range(0, number - 1):
+        xi, yi, zi = sol[:, i]
+        k_1 = f(sigma, r, b, xi, yi, zi)
+        k_2 = f(sigma, r, b, xi + h * k_1[0]/2, yi + h * k_1[1]/2, zi + h * k_1[2]/2)
+        k_3 = f(sigma, r, b, xi + h * k_2[0]/2, yi + h * k_2[1]/2, zi + h * k_2[2]/2)
+        k_4 = f(sigma, r, b, xi + h * k_3[0], yi + h * k_3[1], zi + h * k_3[2])
+        sol[:, i + 1] = sol[:, i] + h * (k_1 + 2 * k_2 + 2 * k_3 + k_4)/6
+    x = sol[0, :]
+    y = sol[1, :]
+    z = sol[2, :]
 
-sol1[0, 0] = x_01
-sol1[1, 0] = y_01
-sol1[2, 0] = z_01
-
-sol2[0, 0] = x_02
-sol2[1, 0] = y_02
-sol2[2, 0] = z_02
-
-
-for i in range(0, number - 1):
-    xi_1, yi_1, zi_1 = sol1[:, i]
-    k_1 = f(sigma, r, b, xi_1, yi_1, zi_1)
-    k_2 = f(sigma, r, b, xi_1 + h * k_1[0]/2, yi_1 + h * k_1[1]/2, zi_1 + h * k_1[2]/2)
-    k_3 = f(sigma, r, b, xi_1 + h * k_2[0]/2, yi_1 + h * k_2[1]/2, zi_1 + h * k_2[2]/2)
-    k_4 = f(sigma, r, b, xi_1 + h * k_3[0], yi_1 + h * k_3[1], zi_1 + h * k_3[2])
-    
-    sol1[:, i + 1] = sol1[:, i] + h * (k_1 + 2 * k_2 + 2 * k_3 + k_4)/6
-    
-x1 = sol1[0, :]
-y1 = sol1[1, :]
-z1 = sol1[2, :]
-
-for i in range(0, number - 1):
-    xi_2, yi_2, zi_2 = sol2[:, i]
-    k_1 = f(sigma, r, b, xi_2, yi_2, zi_2)
-    k_2 = f(sigma, r, b, xi_2 + h * k_1[0]/2, yi_2 + h * k_1[1]/2, zi_2 + h * k_1[2]/2)
-    k_3 = f(sigma, r, b, xi_2 + h * k_2[0]/2, yi_2 + h * k_2[1]/2, zi_2 + h * k_2[2]/2)
-    k_4 = f(sigma, r, b, xi_2 + h * k_3[0], yi_2 + h * k_3[1], zi_2 + h * k_3[2])
-    
-    sol2[:, i + 1] = sol2[:, i] + h * (k_1 + 2 * k_2 + 2 * k_3 + k_4)/6
-    
-x2 = sol2[0, :]
-y2 = sol2[1, :]
-z2 = sol2[2, :]
+    return t, x, y, z
 
 fig = plt.figure(figsize=(10, 8))
 ax = fig.add_subplot(111, projection='3d')
@@ -84,6 +60,9 @@ ax = fig.add_subplot(111, projection='3d')
 ax.set_xlabel('x')
 ax.set_ylabel('y')
 ax.set_zlabel('z')
+
+t, x1, y1, z1 = solve(sigma, r, b, x_01, y_01, z_01, number, T)
+t, x2, y2, z2 = solve(sigma, r, b, x_02, y_02, z_02, number, T)
 
 ax.set_xlim([np.min(x1), np.max(x1)])
 ax.set_ylim([np.min(y1), np.max(y1)])
