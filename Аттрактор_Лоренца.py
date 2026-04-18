42b26926bfb1536f09dcae553314f08e1681747e
Kolya123F <kolyatodorov12@gmail.com>
2026-04-18 21:00:29 +0300

    Сделал код более читаем в файлах Аттрактор_Лоренца и Аттрактор_Лоренца_3D

--- Аттрактор_Лоренца.py
+++ Аттрактор_Лоренца.py
@@ -1,9 +1,10 @@
+#Импорт необходимых библиотек
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 import numpy as np
 
 #Правая часть системы диффуров
-def f1(sigma, x, y, z):
+def f1(sigma, x, y):
     return sigma * (y - x)
 
 def f2(r, x, y, z):
@@ -13,7 +14,10 @@ def f3(b, x, y, z):
     return x * y - b * z
 
 def f(sigma, r, b, x, y, z):
-    return np.array([f1(sigma, x, y, z), f2(r, x, y, z), f3(b, x, y, z)])
+    return np.array([f1(sigma, x, y), f2(r, x, y, z), f3(b, x, y, z)])
+
+#Параметры системы
+sigma, r, b = 10, 28, 8/3
 
 #Начальные условия
 x_0 = 1
@@ -26,8 +30,8 @@ number = 40000
 #Длительность
 T = 30
 
-#Параметры системы
-sigma, r, b = 10, 28, 8/3
+#Шаг дискретизации
+dt = T / number 
 
 #Решаем численно систему диффуром методом Рунге-Кутта
 def solve(sigma, r, b, x0, y0, z0, number, T):
@@ -50,23 +54,24 @@ def solve(sigma, r, b, x0, y0, z0, number, T):
 
     return t, x, y, z
 
+#Решаем систему
 t, x, y, z = solve(sigma, r, b, x_0, y_0, z_0, number, T)
 
+#Создаем область для отрисовки графика
 fig, ax = plt.subplots(2, 1, figsize=(10, 10))
 
+#Обзываем оси
 ax[0].set_xlabel('t')
 ax[0].set_ylabel('x, y, z')
 
+#Устанавливаем границы
 lim_min = np.min([np.min(x), np.min(y), np.min(z)])
 lim_max = np.max([np.max(x), np.max(y), np.max(z)])
 
 ax[0].set_ylim([lim_min, lim_max])
 ax[0].set_xlim([0, T])
 
-animated_line_1, = ax[0].plot([], [], linewidth=0.5, linestyle='-', color='blue', label = '$x(t)$')
-animated_line_2, = ax[0].plot([], [], linewidth=0.5, linestyle='-', color='green', label = '$y(t)$')
-animated_line_3, = ax[0].plot([], [], linewidth=0.5, linestyle='-', color='red', label = '$z(t)$')
-
+#Точки равновесия
 x_pos_1 = round(np.sqrt(b * (r - 1)), 2)
 y_pos_1 = x_pos_1
 z_pos_1 = r - 1
@@ -78,34 +83,54 @@ z_pos_2 = r - 1
 ax[0].plot(t, [x_pos_2] * len(t), linewidth=0.5, linestyle='--')
 ax[0].plot(t, [z_pos_2] * len(t), linewidth=0.5, linestyle='--')
 
+#Создаем анимацию фазовой кривой
+animated_line_1, = ax[0].plot([], [], linewidth=0.5, 
+                              linestyle='-', color='blue', label = '$x(t)$')
 point_1, = ax[0].plot([], [], 'bo', markersize=3)
+
+animated_line_2, = ax[0].plot([], [], linewidth=0.5, 
+                              linestyle='-', color='green', label = '$y(t)$')
 point_2, = ax[0].plot([], [], 'go', markersize=3)
+
+animated_line_3, = ax[0].plot([], [], linewidth=0.5, 
+                              linestyle='-', color='red', label = '$z(t)$')
 point_3, = ax[0].plot([], [], 'ro', markersize=3)
 
+#Фурье-преобразование (метод FFT) от x(t), y(t), z(t)
 fourier_x = np.fft.fft(x, axis = 0)
-fourier_y = np.fft.fft(y, axis = 0)
-fourier_z = np.fft.fft(z, axis = 0)
-
 amplitude_spectrum_x = np.abs(fourier_x)
-amplitude_spectrum_y = np.abs(fourier_y)
-amplitude_spectrum_z = np.abs(fourier_z)
-
 frequencies_x = np.fft.fftfreq(len(x), t[1] - t[0])
+
+fourier_y = np.fft.fft(y, axis = 0)
+amplitude_spectrum_y = np.abs(fourier_y)
 frequencies_y = np.fft.fftfreq(len(y), t[1] - t[0])
+
+fourier_z = np.fft.fft(z, axis = 0)
+amplitude_spectrum_z = np.abs(fourier_z)
 frequencies_z = np.fft.fftfreq(len(z), t[1] - t[0])
 
 def update(frame):
     animated_line_1.set_data(t[:frame], x[:frame])
-    animated_line_2.set_data(t[:frame], y[:frame])
-    animated_line_3.set_data(t[:frame], z[:frame])
-    
     point_1.set_data([t[frame]], [x[frame]])
+
+    animated_line_2.set_data(t[:frame], y[:frame])
     point_2.set_data([t[frame]], [y[frame]])
+
+    animated_line_3.set_data(t[:frame], z[:frame])
     point_3.set_data([t[frame]], [z[frame]])
     
-    ax[0].set_title(f'Аттрактор Лоренца: $\sigma = 10 \\text{{, }} r = 28 \\text{{, }} b = 8/3$\nВремя: {t[frame]:.2f} из {t[-1]:.1f}')
+    ax[0].set_title(f'Аттрактор Лоренца:\n'
+                    f'$\sigma = 10 \\text{{, }}$'
+                    f'$r = 28 \\text{{, }}$'
+                    f'$b = 8/3$\n'
+                    f'Шаг дискретизации: ${dt}$\n'
+                    f'Время: {t[frame]:.2f} из {t[-1]:.1f}')
     
-    return animated_line_1, point_1, animated_line_2, point_2, animated_line_3, point_3
+    return (
+        animated_line_1, point_1, 
+        animated_line_2, point_2, 
+        animated_line_3, point_3
+        )
 
 animation = FuncAnimation(
     fig=fig, 
@@ -118,14 +143,25 @@ animation = FuncAnimation(
 
 #animation.save('lorenz_attractor.gif', writer='pillow', fps=60)  # Для GIF
 #animation.save('lorenz_attractor.mp4', writer='ffmpeg', fps=30)  # Для MP4
-ax[1].plot(frequencies_x, amplitude_spectrum_x, 'b-', alpha=0.7, label='Преобразование Фурье от x(t)', linewidth=0.5)
-ax[1].plot(frequencies_y, amplitude_spectrum_y, 'g-', alpha=0.7, label='Преобразование Фурье от y(t)', linewidth=0.5)
-ax[1].plot(frequencies_z, amplitude_spectrum_z, 'r-', alpha=0.7, label='Преобразование Фурье от z(t)', linewidth=0.5)
+
+#Отрисовываем фурье-спектры
+ax[1].plot(frequencies_x, amplitude_spectrum_x, 'b-', 
+           alpha=0.7, label='Преобразование Фурье от x(t)', linewidth=0.5)
+ax[1].plot(frequencies_y, amplitude_spectrum_y, 'g-', 
+           alpha=0.7, label='Преобразование Фурье от y(t)', linewidth=0.5)
+ax[1].plot(frequencies_z, amplitude_spectrum_z, 'r-', 
+           alpha=0.7, label='Преобразование Фурье от z(t)', linewidth=0.5)
+
+#Лог. масштаб по оси Y для фурье-спектра
 ax[1].set_yscale('log')
+
+#Обзываем оси для фурье-спектра
 ax[1].set_xlabel('$\omega$')
 ax[1].set_ylabel('$A$')
+
 ax[1].legend()
 ax[1].grid()
+
 ax[0].legend()
 ax[0].grid()
 plt.show()
--- Аттрактор_Лоренца_3D.py
+++ Аттрактор_Лоренца_3D.py
@@ -1,9 +1,10 @@
+#Импорт необходимых библиотек
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 import numpy as np
 
 #Правая часть системы диффуров
-def f1(sigma, x, y, z):
+def f1(sigma, x, y):
     return sigma * (y - x)
 
 def f2(r, x, y, z):
@@ -13,24 +14,13 @@ def f3(b, x, y, z):
     return x * y - b * z
 
 def f(sigma, r, b, x, y, z):
-    return np.array([f1(sigma, x, y, z), f2(r, x, y, z), f3(b, x, y, z)])
-
-#Число точек
-number = 100000
-
-#Длительность
-T = 30
+    return np.array([f1(sigma, x, y), f2(r, x, y, z), f3(b, x, y, z)])
 
 #Параметры системы
 sigma, r, b = 10, 28, 8/3
 
-x_pos_1 = round(np.sqrt(b * (r - 1)), 2)
-y_pos_1 = x_pos_1
-z_pos_1 = r - 1
-
-x_pos_2 = -round(np.sqrt(b * (r - 1)), 2)
-y_pos_2 = x_pos_2
-z_pos_2 = r - 1
+#Число точек
+number = 100000
 
 #Начальные условия
 x_01 = 1
@@ -40,6 +30,12 @@ z_01 = 1
 x_02 = 1 + 0.00001
 y_02 = 1
 z_02 = 1
+
+#Длительность
+T = 30
+
+#Шаг дискретизации
+dt = T / number 
 
 #Решаем численно систему диффуром методом Рунге-Кутта
 def solve(sigma, r, b, x0, y0, z0, number, T):
@@ -52,9 +48,12 @@ def solve(sigma, r, b, x0, y0, z0, number, T):
     for i in range(0, number - 1):
         xi, yi, zi = sol[:, i]
         k_1 = f(sigma, r, b, xi, yi, zi)
-        k_2 = f(sigma, r, b, xi + h * k_1[0]/2, yi + h * k_1[1]/2, zi + h * k_1[2]/2)
-        k_3 = f(sigma, r, b, xi + h * k_2[0]/2, yi + h * k_2[1]/2, zi + h * k_2[2]/2)
-        k_4 = f(sigma, r, b, xi + h * k_3[0], yi + h * k_3[1], zi + h * k_3[2])
+        k_2 = f(sigma, r, b, xi + h * k_1[0]/2, yi + h * k_1[1]/2, 
+                zi + h * k_1[2]/2)
+        k_3 = f(sigma, r, b, xi + h * k_2[0]/2, yi + h * k_2[1]/2, 
+                zi + h * k_2[2]/2)
+        k_4 = f(sigma, r, b, xi + h * k_3[0], yi + h * k_3[1], 
+                zi + h * k_3[2])
         sol[:, i + 1] = sol[:, i] + h * (k_1 + 2 * k_2 + 2 * k_3 + k_4)/6
     x = sol[0, :]
     y = sol[1, :]
@@ -62,22 +61,29 @@ def solve(sigma, r, b, x0, y0, z0, number, T):
 
     return t, x, y, z
 
+#Решения с разными начальными условиями 
+t, x1, y1, z1 = solve(sigma, r, b, x_01, y_01, z_01, number, T)
+t, x2, y2, z2 = solve(sigma, r, b, x_02, y_02, z_02, number, T)
+
+#Создаем область для отрисовки графиков
 fig = plt.figure(figsize=(10, 8))
+
 ax = fig.add_subplot(121, projection='3d')
 ax1 = fig.add_subplot(122)
 
+#Обзываем оси для фазовой кривой в 3D 
 ax.set_xlabel('x')
 ax.set_ylabel('y')
 ax.set_zlabel('z')
 
+#Обзываем оси для временной зависимости расстояния между решениями
 ax1.set_xlabel('t')
 ax1.set_ylabel('dist')
 
-t, x1, y1, z1 = solve(sigma, r, b, x_01, y_01, z_01, number, T)
-t, x2, y2, z2 = solve(sigma, r, b, x_02, y_02, z_02, number, T)
-
+#Расстояние между решениями
 dist = np.sqrt((x1-x2) ** 2 + (y1-y2) ** 2 + (z1 - z2) ** 2)
 
+#Устанавливаем границы для графиков
 ax.set_xlim([np.min(x1), np.max(x1)])
 ax.set_ylim([np.min(y1), np.max(y1)])
 ax.set_zlim([np.min(z1), np.max(z1)])
@@ -85,33 +91,55 @@ ax.set_zlim([np.min(z1), np.max(z1)])
 ax1.set_xlim([0, T])
 ax1.set_ylim([-1, np.max(dist)])
 
-animated_line_1, = ax.plot([], [], [], linewidth=0.5, linestyle='--', color='blue', label = r'$x_0 = 1 \text{, } y_0 = 1 \text{, } z_0 = 1$')
-animated_line_2, = ax.plot([], [], [], linewidth=0.5, linestyle='-', color='green', label = r'$x_0 = 1 + 10^{-5}  \text{, } y_0 = 1 \text{, } z_0 = 1$')
+animated_line_1, = ax.plot([], [], [], linewidth=0.5, linestyle='--', 
+                           color='blue', 
+                           label = (r'$x_0 = 1'
+                                    r' \text{, } y_0 = 1 \text{, } z_0 = 1$'))
+point_1, = ax.plot([], [], [], 'bo', markersize=3)
+
+animated_line_2, = ax.plot([], [], [], linewidth=0.5, linestyle='-', 
+                           color='green', 
+                           label = (r'$x_0 = 1 + 10^{-5}'  
+                                    r'\text{, } y_0 = 1 \text{, } z_0 = 1$'))
+point_2, = ax.plot([], [], [], 'go', markersize=3)
 
 animated_line, = ax1.plot([], [], linewidth=0.5, linestyle='-', color='black')
 
-equilibrium_positiona_1 = ax.plot([x_pos_1], [y_pos_1], [z_pos_1], 'o', color = 'black', markersize=4, label = f'{(x_pos_1, y_pos_1, z_pos_1)}')
+#Точки равновесия
+x_pos_1 = round(np.sqrt(b * (r - 1)), 2)
+y_pos_1 = x_pos_1
+z_pos_1 = r - 1
 
-equilibrium_positiona_2 = ax.plot([x_pos_2], [y_pos_2], [z_pos_2], 'o', color = 'gray', markersize=4, label = f'{(x_pos_2, y_pos_2, z_pos_2)}')
+x_pos_2 = -round(np.sqrt(b * (r - 1)), 2)
+y_pos_2 = x_pos_2
+z_pos_2 = r - 1
 
-point_1, = ax.plot([], [], [], 'bo', markersize=3)
-point_2, = ax.plot([], [], [], 'go', markersize=3)
+equilibrium_positiona_1 = ax.plot([x_pos_1], [y_pos_1], [z_pos_1], 'o', 
+                                  color = 'black', markersize=4, 
+                                  label = f'{(x_pos_1, y_pos_1, z_pos_1)}')
+
+equilibrium_positiona_2 = ax.plot([x_pos_2], [y_pos_2], [z_pos_2], 'o', 
+                                  color = 'gray', markersize=4, 
+                                  label = f'{(x_pos_2, y_pos_2, z_pos_2)}')
 
 def update(frame):
     animated_line_1.set_data(x1[:frame], y1[:frame])
-    animated_line_2.set_data(x2[:frame], y2[:frame])
     animated_line_1.set_3d_properties(z1[:frame])
-    animated_line_2.set_3d_properties(z2[:frame])
-
-    animated_line.set_data(t[:frame], dist[:frame])
-    
     point_1.set_data([x1[frame]], [y1[frame]])
-    point_2.set_data([x2[frame]], [y2[frame]])
     point_1.set_3d_properties([z1[frame]])
+
+    animated_line_2.set_data(x2[:frame], y2[:frame])
+    animated_line_2.set_3d_properties(z2[:frame])
+    point_2.set_data([x2[frame]], [y2[frame]])
     point_2.set_3d_properties([z2[frame]])
 
-    ax.set_title(f'Аттрактор Лоренца: $\sigma = {sigma} \\text{{, }} r = {r} \\text{{, }} b = {round(b, 2)}$\nВремя: {t[frame]:.2f} из {t[-1]:.1f}')
-    
+    animated_line.set_data(t[:frame], dist[:frame])
+    ax.set_title(f'Аттрактор Лоренца:\n'
+                f'$\sigma = 10 \\text{{, }}$'
+                f'$r = 28 \\text{{, }}$'
+                f'$b = 8/3$\n'
+                f'Шаг дискретизации: ${dt}$\n'
+                f'Время: {t[frame]:.2f} из {t[-1]:.1f}')
     return animated_line_1, point_1, animated_line_2, point_2, animated_line
 
 animation = FuncAnimation(
